3abbf792d13a726f7d5941abf3a7dbff3209c4c9
usebook <usebook@outlook.com>
2018-01-10 23:32:37 +0800

    家里编程

--- UseBook/Form1.cs
+++ UseBook/Form1.cs
@@ -6,7 +6,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
+
 
 namespace UseBook
 {
@@ -17,14 +22,27 @@ namespace UseBook
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-        }
 
-        private void 导出ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
 
+        private void button1_Click(object sender, EventArgs e)
+        {       
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            Workbooks wbks = app.Workbooks;
+            _Workbook _wbk = wbks.Add(System.IO.Directory.GetCurrentDirectory()+"\\excel\\45F-48F座位图.xlsx");
+
+            Sheets shs = _wbk.Sheets;
+            _Worksheet[] _wsh = new Worksheet[4];
+            for (int i=0; i<_wbk.Sheets.Count; i++)
+            {
+                _wsh[i] = (_Worksheet)shs.get_Item(i+1);
+            }
+
+            MessageBox.Show(_wsh[0].Range.g);
+
+
+            MessageBox.Show("等一下");
+            
         }
     }
 }
